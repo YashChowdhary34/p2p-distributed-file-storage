a90221700ff8f97ffb2a852334f5e160c29b376e
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-05-22 10:26:39 +0530

    implemented delete folder for cleanup

--- store_test.go
+++ store_test.go
@@ -21,6 +21,23 @@ func TestPathTransformFunc(t *testing.T) {
 	}
 }
 
+func TestStoreDelete(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "momsspecials"
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
 		PathTransformFunc: CASPathTransformFunc,
@@ -45,4 +62,6 @@ func TestStore(t *testing.T) {
 	if string(b) != string(data) {
 		t.Errorf("want %s have %s", data, b)
 	}
+
+	s.Delete(key)
 }
